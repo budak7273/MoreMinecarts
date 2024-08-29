7dbf32d5a2d6841649d7ddbd0324e11d65888749
Rob B <computerguy440+gh@gmail.com>
2024-08-29 18:55:26 -0400

    Avoid adding craft entries multiple times

--- MoreMinecartsTweaks.cs
+++ MoreMinecartsTweaks.cs
@@ -1,6 +1,7 @@
 using CoreLib.Submodules.ModEntity.Atributes;
 using MoreMinecarts;
 using PugMod;
+using System.Linq;
 using Unity.Entities;
 using UnityEngine;
 
@@ -12,20 +13,28 @@ public static class MoreMinecartsTweaks
     {
         var canCraftBuffer = entityManager.GetBuffer<CanCraftObjectsBuffer>(entity);
 
-        addBufferEntry(canCraftBuffer, API.Authoring.GetObjectID("MoreMinecarts:CartScarlet"));
-        addBufferEntry(canCraftBuffer, API.Authoring.GetObjectID("MoreMinecarts:CartSolar"));
+        addBufferEntry(canCraftBuffer, API.Authoring.GetObjectID("MoreMinecarts:CartScarlet"), 1);
+        addBufferEntry(canCraftBuffer, API.Authoring.GetObjectID("MoreMinecarts:CartSolar"), 1);
         //addBufferEntry(canCraftBuffer, API.Authoring.GetObjectID("MoreMinecarts:CartInvisible"));
         //addBufferEntry(canCraftBuffer, API.Authoring.GetObjectID("MoreMinecarts:CartLegendaryParchment"));
     }
 
-    private static void addBufferEntry(DynamicBuffer<CanCraftObjectsBuffer> canCraftBuffer, ObjectID itemId)
+    private static void addBufferEntry(DynamicBuffer<CanCraftObjectsBuffer> canCraftBuffer, ObjectID itemId, int outputAmount)
     {
-        Debug.Log($"[{MoreMinecartsMod.NAME}]: Adding itemId {itemId} to crafter");
-        canCraftBuffer.Add(new CanCraftObjectsBuffer
+        CanCraftObjectsBuffer entry = new CanCraftObjectsBuffer
         {
             objectID = itemId,
-            amount = 1,
+            amount = outputAmount,
             entityAmountToConsume = 0
-        });
+        };
+        if (canCraftBuffer.Contains(entry))
+        {
+            Debug.Log($"[{MoreMinecartsMod.NAME}]: Crafter already contained itemId {itemId} so not adding it again");
+        }
+        else
+        {
+            canCraftBuffer.Add(entry);
+            Debug.Log($"[{MoreMinecartsMod.NAME}]: Adding itemId {itemId} to crafter");
+        }
     }
 }
